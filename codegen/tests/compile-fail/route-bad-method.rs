85c06fde046a798f3a534582db89766bbeb55f96
Sergio Benitez <sb@sergio.bz>
2016-09-28 20:55:13 -0700

    Fix test for new error message.

--- codegen/tests/compile-fail/route-bad-method.rs
+++ codegen/tests/compile-fail/route-bad-method.rs
@@ -3,7 +3,7 @@
 
 extern crate rocket;
 
-#[route(FIX, "hello")]  //~ ERROR FIX is not a valid HTTP method
+#[route(FIX, "hello")]  //~ ERROR is not a valid HTTP method
 //~^ ERROR valid HTTP method
 fn get() -> &'static str { "hi" }
 
