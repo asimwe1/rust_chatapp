6498c2647386c6641d57c24c7a4862686b6ae285
Sergio Benitez <sb@sergio.bz>
2016-09-28 19:38:11 -0700

    Fix test for latest nightly.

--- codegen/tests/compile-fail/malformed-param-list.rs
+++ codegen/tests/compile-fail/malformed-param-list.rs
@@ -5,21 +5,21 @@
 fn get() -> &'static str { "hi" }
 
 #[get("/<name><")] //~ ERROR malformed
-fn get(name: &str) -> &'static str { "hi" }
+fn get1(name: &str) -> &'static str { "hi" }
 
 #[get("/<<<<name><")] //~ ERROR identifiers
-fn get(name: &str) -> &'static str { "hi" }
+fn get2(name: &str) -> &'static str { "hi" }
 
 #[get("/<!>")] //~ ERROR identifiers
-fn get() -> &'static str { "hi" }
+fn get3() -> &'static str { "hi" }
 
 #[get("/<_>")] //~ ERROR ignored
-fn get() -> &'static str { "hi" }
+fn get4() -> &'static str { "hi" }
 
 #[get("/<1>")] //~ ERROR identifiers
-fn get() -> &'static str { "hi" }
+fn get5() -> &'static str { "hi" }
 
 #[get("/<>name><")] //~ ERROR cannot be empty
-fn get() -> &'static str { "hi" }
+fn get6() -> &'static str { "hi" }
 
 fn main() {  }
