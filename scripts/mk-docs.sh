dc677124c6ac2110c26dc8de14381f9d86e17bf9
Sergio Benitez <sb@sergio.bz>
2016-10-02 17:29:59 -0700

    Add script to build docs.

---
+++ scripts/mk-docs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+#
+# Builds the rustdocs for all of the libraries.
+#
+
+# Brings in: EXAMPLES_DIR, LIB_DIR, CODEGEN_DIR, and CONTRIB_DIR, DOC_DIR
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/config.sh
+
+function mk_doc() {
+  local dir=$1
+  pushd $dir > /dev/null
+    echo ":: Documenting '${dir}'..."
+    cargo doc --no-deps --all-features
+  popd > /dev/null
+}
+
+mk_doc $LIB_DIR
+mk_doc $CODEGEN_DIR
+mk_doc $CONTRIB_DIR
