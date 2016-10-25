81113ecf504688fad938a3f499de6adb75397341
Sergio Benitez <sb@sergio.bz>
2016-10-25 13:35:02 +0200

    Remove public qualification from private data modules.

--- lib/src/data/mod.rs
+++ lib/src/data/mod.rs
@@ -1,8 +1,8 @@
 //! Types and traits for reading and parsing request body data.
 
-#[cfg(any(test, feature = "testing"))] pub mod test_data;
-#[cfg(not(any(test, feature = "testing")))] pub mod data;
-#[cfg(not(any(test, feature = "testing")))] pub mod data_stream;
+#[cfg(any(test, feature = "testing"))] mod test_data;
+#[cfg(not(any(test, feature = "testing")))] mod data;
+#[cfg(not(any(test, feature = "testing")))] mod data_stream;
 mod from_data;
 
 pub use self::from_data::{FromData, Outcome};
