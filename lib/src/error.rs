dd030334e4062919333fc502df11318b67b8bb16
Sergio Benitez <sb@sergio.bz>
2016-11-03 19:00:52 +0100

    Document the Error type.

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -1,8 +1,14 @@
+/// [unstable] Error type for Rocket. Likely to change.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub enum Error {
+    /// The request method was bad.
     BadMethod,
+    /// The value could not be parsed.
     BadParse,
+    /// There was no such route.
     NoRoute, // TODO: Add a chain of routes attempted.
+    /// The error was internal.
     Internal,
+    /// The requested key/index does not exist.
     NoKey,
 }
