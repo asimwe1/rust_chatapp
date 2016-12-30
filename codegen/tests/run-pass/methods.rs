366eb5d158c7fe6486426403f2127740e9c29295
Sergio Benitez <sb@sergio.bz>
2016-12-30 01:46:47 -0600

    Test methods via route attribute.

--- codegen/tests/run-pass/methods.rs
+++ codegen/tests/run-pass/methods.rs
@@ -3,26 +3,26 @@
 
 extern crate rocket;
 
-#[get("/")]
-fn get() {  }
+#[get("/")] fn get() {  }
+#[route(GET, "/")] fn get_r() {  }
 
-#[put("/")]
-fn put() {  }
+#[put("/")] fn put() {  }
+#[route(PUT, "/")] fn put_r() {  }
 
-#[post("/")]
-fn post() {  }
+#[post("/")] fn post() {  }
+#[route(POST, "/")] fn post_r() {  }
 
-#[delete("/")]
-fn delete() {  }
+#[delete("/")] fn delete() {  }
+#[route(DELETE, "/")] fn delete_r() {  }
 
-#[head("/")]
-fn head() {  }
+#[head("/")] fn head() {  }
+#[route(HEAD, "/")] fn head_r() {  }
 
-#[patch("/")]
-fn patch() {  }
+#[patch("/")] fn patch() {  }
+#[route(PATCH, "/")] fn patch_r() {  }
 
 // TODO: Allow this once Diesel incompatibility is fixed.
-// #[options("/")]
-// fn options() {  }
+// #[options("/")] fn options() {  }
+#[route(OPTIONS, "/")] fn options_r() {  }
 
 fn main() { }
