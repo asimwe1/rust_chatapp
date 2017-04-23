6f5b840d0081f5b13464baf11b5c6b616a02f7d0
Sergio Benitez <sb@sergio.bz>
2017-04-23 00:03:14 -0700

    Remove now-unneeded doc shenanigans.

--- scripts/mk-docs.sh
+++ scripts/mk-docs.sh
@@ -12,11 +12,10 @@ source $SCRIPT_DIR/config.sh
 function mk_doc() {
   local dir=$1
   local flag=$2
-  pushd $dir > /dev/null
+  pushd $dir > /dev/null 2>&1
     echo ":: Documenting '${dir}'..."
     cargo doc --no-deps --all-features
-    cargo doc --no-deps $flag
-  popd > /dev/null
+  popd > /dev/null 2>&1
 }
 
 # We need to clean-up beforehand so we don't get all of the dependencies.
@@ -24,7 +23,7 @@ cargo clean
 
 mk_doc $LIB_DIR
 mk_doc $CODEGEN_DIR
-mk_doc $CONTRIB_DIR  --all-features
+mk_doc $CONTRIB_DIR
 
 # Blank index, for redirection.
 touch ${DOC_DIR}/index.html
