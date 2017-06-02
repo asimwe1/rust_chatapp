f1ec552ac567209d34c9cdcc16e52781797b3712
Sergio Benitez <sb@sergio.bz>
2017-06-01 21:59:06 -0700

    Make todo example bootstrapping more resilient.

--- examples/todo/bootstrap.sh
+++ examples/todo/bootstrap.sh
@@ -4,6 +4,9 @@ SCRIPT_PATH=$(cd "$(dirname "$0")" ; pwd -P)
 DATABASE_URL=${SCRIPT_PATH}/db/db.sql
 
 pushd $SCRIPT_PATH > /dev/null
+  # clear an existing database
+  rm -f $DATABASE_URL
+
   # install the diesel CLI tools if they're not installed
   if ! command -v diesel >/dev/null 2>&1; then
     cargo install diesel_cli --no-default-features --features=sqlite > /dev/null
