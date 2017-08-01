3a76fe03db9f5da7f9304f6ba731e2a6cce71d1d
Rolf Sievers <rolf.sievers@posteo.de>
2017-08-01 20:38:02 +0200

    Improve commentary on collisions in pastebin example.

--- examples/pastebin/src/paste_id.rs
+++ examples/pastebin/src/paste_id.rs
@@ -14,8 +14,8 @@ pub struct PasteID<'a>(Cow<'a, str>);
 impl<'a> PasteID<'a> {
     /// Generate a _probably_ unique ID with `size` characters. For readability,
     /// the characters used are from the sets [0-9], [A-Z], [a-z]. The
-    /// probability of a collision depends on the value of `size`. In
-    /// particular, the probability of a collision is 1/62^(size).
+    /// probability of a collision depends on the value of `size` and the number
+    /// of IDs generated thus far.
     pub fn new(size: usize) -> PasteID<'static> {
         let mut id = String::with_capacity(size);
         let mut rng = rand::thread_rng();
