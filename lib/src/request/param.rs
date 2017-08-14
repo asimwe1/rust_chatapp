f66780bd9e3728d273c6b4b759478380e18150d7
adrian5 <adrian5@users.noreply.github.com>
2017-08-14 18:49:09 +0200

    Add missing comma in 'FromParam' docs.

--- lib/src/request/param.rs
+++ lib/src/request/param.rs
@@ -75,7 +75,7 @@ use http::RawStr;
 /// Rocket implements `FromParam` for several standard library types. Their
 /// behavior is documented here.
 ///
-///   * **f32, f64, isize, i8, i16, i32, i64, usize, u8, u16, u32, u64, bool
+///   * **f32, f64, isize, i8, i16, i32, i64, usize, u8, u16, u32, u64, bool,
 ///     IpAddr, Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6, SocketAddr**
 ///
 ///     A value is parsed successfully if the `from_str` method from the given
