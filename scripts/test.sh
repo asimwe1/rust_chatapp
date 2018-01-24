f0af77d917875fc67555a6321905ea64f0692ca0
Sergio Benitez <sb@sergio.bz>
2018-01-23 18:49:59 -0800

    Disable incremental compilation in testing script.
    With incremental compilation, compilation output disk usage grows to
    almost 10GB. This prevent that from happening.

--- scripts/test.sh
+++ scripts/test.sh
@@ -83,4 +83,4 @@ echo ":: Bootstrapping examples..."
 bootstrap_examples
 
 echo ":: Building and testing libraries..."
-cargo test --all-features --all $@
+CARGO_INCREMENTAL=0 cargo test --all-features --all $@
