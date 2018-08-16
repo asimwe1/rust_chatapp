01a5011b610588639094977c246b77a31727cf4c
Sergio Benitez <sb@sergio.bz>
2018-08-15 19:55:34 -0700

    Add a module-level docstring to 'http'.

--- core/lib/src/lib.rs
+++ core/lib/src/lib.rs
@@ -130,6 +130,11 @@ pub mod error;
 
 // Reexport of HTTP everything.
 pub mod http {
+    //! Types that map to concepts in HTTP.
+    //!
+    //! This module exports types that map to HTTP concepts or to the underlying
+    //! HTTP library when needed.
+
     #[doc(inline)]
     pub use rocket_http::*;
 }
