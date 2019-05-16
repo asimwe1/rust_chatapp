868a7e642534975a4b374dcdd3909f33a1ccf867
Sergio Benitez <sb@sergio.bz>
2019-05-16 13:29:06 -0700

    Update 'crossbeam' to 0.7 in 'managed_queue' example.

--- examples/managed_queue/src/main.rs
+++ examples/managed_queue/src/main.rs
@@ -6,9 +6,9 @@ extern crate crossbeam;
 #[cfg(test)] mod tests;
 
 use rocket::State;
-use crossbeam::queue::MsQueue;
+use crossbeam::queue::SegQueue;
 
-struct LogChannel(MsQueue<String>);
+struct LogChannel(SegQueue<String>);
 
 #[put("/push?<event>")]
 fn push(event: String, queue: State<LogChannel>) {
@@ -16,14 +16,14 @@ fn push(event: String, queue: State<LogChannel>) {
 }
 
 #[get("/pop")]
-fn pop(queue: State<LogChannel>) -> String {
-    queue.0.pop()
+fn pop(queue: State<LogChannel>) -> Option<String> {
+    queue.0.pop().ok()
 }
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![push, pop])
-        .manage(LogChannel(MsQueue::new()))
+        .manage(LogChannel(SegQueue::new()))
 }
 
 fn main() {
