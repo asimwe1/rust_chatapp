6321797a746b4bcaa950b34a0a2f8e0dc1684ca7
Jeb Rosen <jeb@jebrosen.com>
2019-11-24 09:15:09 -0800

    Update compile error tests for databases.

--- contrib/codegen/tests/ui-fail/database-types.rs
+++ contrib/codegen/tests/ui-fail/database-types.rs
@@ -4,10 +4,16 @@ extern crate rocket;
 struct Unknown;
 
 #[database("foo")]
+//~^ ERROR Unknown: rocket_contrib::databases::Poolable
+//~^^ ERROR is private
+//~^^^ ERROR no method named `get`
 struct A(Unknown);
 //~^ ERROR Unknown: rocket_contrib::databases::Poolable
 
 #[database("foo")]
+//~^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
+//~^^ ERROR is private
+//~^^^ ERROR no method named `get`
 struct B(Vec<i32>);
 //~^ ERROR Vec<i32>: rocket_contrib::databases::Poolable
 
