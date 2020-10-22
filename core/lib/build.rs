17fd7f4286c3bb8ecbc33ce5c693b5a42f31e63e
Sergio Benitez <sb@sergio.bz>
2020-10-21 19:56:12 -0700

    Update minimum rustc to 1.46.

--- core/lib/build.rs
+++ core/lib/build.rs
@@ -2,20 +2,15 @@
 
 use yansi::{Paint, Color::{Red, Yellow}};
 
-const MIN_VERSION: &'static str = "1.45.0";
-
-macro_rules! err {
-    ($version:expr, $msg:expr) => (
-        eprintln!("{} {}", Red.paint("Error:").bold(), Paint::new($msg).bold());
-        eprintln!("Installed version: {}", Yellow.paint(format!("{}", $version)));
-        eprintln!("Minimum required:  {}", Yellow.paint(format!("{}", MIN_VERSION)));
-    )
-}
-
 fn main() {
+    const MIN_VERSION: &'static str = "1.46.0";
+
     if let Some(version) = version_check::Version::read() {
         if !version.at_least(MIN_VERSION) {
-            err!(version, "Rocket requires a more recent version of rustc.");
+            let msg = "Rocket requires a more recent version of rustc.";
+            eprintln!("{} {}", Red.paint("Error:").bold(), Paint::new(msg).bold());
+            eprintln!("Installed version: {}", Yellow.paint(format!("{}", version)));
+            eprintln!("Minimum required:  {}", Yellow.paint(format!("{}", MIN_VERSION)));
             panic!("Aborting compilation due to incompatible compiler.")
         }
     } else {
