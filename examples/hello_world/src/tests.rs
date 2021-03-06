fe9e2256a343ac8608a3016b63cf991ec1e026c4
Sergio Benitez <sb@sergio.bz>
2021-03-06 01:39:24 -0800

    Update hello_world example test.

--- examples/hello_world/src/tests.rs
+++ examples/hello_world/src/tests.rs
@@ -5,4 +5,14 @@ fn hello_world() {
     let client = Client::tracked(super::rocket()).unwrap();
     let response = client.get("/").dispatch();
     assert_eq!(response.into_string(), Some("Hello, world!".into()));
+
+    let response = client.get("/hello/world").dispatch();
+    assert_eq!(response.into_string(), Some("Hello, world!".into()));
+}
+
+#[test]
+fn hello_mir() {
+    let client = Client::tracked(super::rocket()).unwrap();
+    let response = client.get("/hello/%D0%BC%D0%B8%D1%80").dispatch();
+    assert_eq!(response.into_string(), Some("Привет, мир!".into()));
 }
