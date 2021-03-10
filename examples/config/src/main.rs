015438a7806b58532ba56293a5459c9ea4d5c267
Sergio Benitez <sb@sergio.bz>
2021-03-10 03:37:03 -0800

    Print config on attach in config example.

--- examples/config/src/main.rs
+++ examples/config/src/main.rs
@@ -1,5 +1,15 @@
 #[cfg(test)] mod tests;
 
-// This example's illustration is the Rocket.toml file.
+use rocket::fairing::AdHoc;
+
+// This example's illustration is the Rocket.toml file. Running this server will
+// print the config, however.
 #[rocket::launch]
-fn rocket() -> rocket::Rocket { rocket::ignite() }
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .attach(AdHoc::on_attach("Config Reader", |rocket| async {
+            let value = rocket.figment().find_value("").unwrap();
+            println!("{:#?}", value);
+            Ok(rocket)
+        }))
+}
