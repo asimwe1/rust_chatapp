f3e08bf3f635f8d61f5de9352d33318a1e1d5e1f
Sergio Benitez <sb@sergio.bz>
2021-06-26 12:01:56 -0700

    Add Markdown (text/markdown) as a known media type.

--- core/http/src/header/known_media_types.rs
+++ core/http/src/header/known_media_types.rs
@@ -44,6 +44,7 @@ macro_rules! known_media_types {
         MP4 (is_mp4): "MPEG4 Video", "video", "mp4",
         ZIP (is_zip): "ZIP archive", "application", "zip",
         EventStream (is_event_stream): "SSE stream", "text", "event-stream",
+        Markdown (is_markdown): "markdown text", "text", "markdown" ; "charset" => "utf-8",
     })
 }
 
@@ -91,6 +92,8 @@ macro_rules! known_extensions {
         "tiff" => TIFF,
         "mov" => MOV,
         "zip" => ZIP,
+        "md" => Markdown,
+        "markdown" => Markdown,
     })
 }
 
@@ -110,5 +113,7 @@ macro_rules! known_shorthands {
         "multipart" => FormData,
         "xml" => XML,
         "pdf" => PDF,
+        "markdown" => Markdown,
+        "md" => Markdown,
     })
 }
